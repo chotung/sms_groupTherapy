8e91a9e47fc640e6928e2d71e2d1ede4f9e0091a
chotung <38818833+chotung@users.noreply.github.com>
2021-06-09 21:14:23 -0400

    can send a text to mobile

--- index.js
+++ index.js
@@ -3,34 +3,59 @@ require("dotenv").config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 // const client = require("twilio")(accountSid, authToken);
-const session = require("express-session");
-const MessagingResponse = require("twilio").twiml.MessagingResponse;
+// const session = require("express-session");
+// const MessagingResponse = require("twilio").twiml.MessagingResponse;
 const client = require("twilio")(accountSid, authToken);
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-	session({
-		secret: "anything-you-want-but-keep-secret",
-		resave: false,
-		saveUninitialized: false,
-	})
-);
-const port = process.env.PORT || 1337;
+// app.use(
+// 	session({
+// 		secret: "anything-you-want-but-keep-secret",
+// 		resave: false,
+// 		saveUninitialized: false,
+// 	})
+// );
+const port = process.env.PORT || 3000;
 
-app.post("/", async (req, res) => {
-	const messageFromApp = req.body.message;
-	// send to phone
-	if (messageFromApp) {
-		const message = await client.messages.create({
-			body: messageFromApp,
-			from: process.env.TWILIO_NUMBER,
-			to: process.env.MOBILE_NUMBER,
-		});
-		res.json(message);
-	}
-	// .then((message) => console.log(message));
+// client.messages
+// 	.create({ body: "Hi there", from: "+15017122661", to: "+15558675310" })
+// 	.then((message) => console.log(message.sid));
+
+// SEND TO USER
+app.post("/reply", async (req, res) => {
+	const mes = req.body.message;
+	// FIGURE OUT WHO I AM REPLYING TO
+	const message = await client.messages.create({
+		body: mes,
+		from: process.env.TWILIO_NUMBER,
+		to: process.env.MOBILE_NUMBER,
+	});
+	// FIND CONVERSATION
+	// ADD MESSAGE TO CONVERSATION
+	// ALWAYS UPDATE THE CONVERSATION
+	res.json(message);
+});
+
+// Receive
+app.get("/sms", async (req, res) => {
+	const body = req.body.Body;
+	// SAVE text and number
+	// CREATE new convo
+	// assign the the number and twilio number as participants
+	console.log(body);
+
+	// Start our TwiML response.
+	// const twiml = new MessagingResponse();
+
+	// Add a text message.
+	// const msg = twiml.message(
+	// 	"Hey Phone thanks for the text here's a return text"
+	// );
+
+	// res.writeHead(200, { "Content-Type": "text/xml" });
+	// res.end(twiml.toString());
 });
 
 app.listen(port, () => {
